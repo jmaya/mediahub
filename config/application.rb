39f5a3c1f6672233b5da3f3b994ac50206a4cd98
John Maya <jcmaya@hotmail.com>
2015-03-24 14:49:22 -0400

    renamed

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MediaManager
+module MediaHub
   class Application < Rails::Application
     config.i18n.enforce_available_locales = true
 
