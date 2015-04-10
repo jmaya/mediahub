ad759ad81e521b8de3274da59b7b42f521641889
John Maya <jcmaya@hotmail.com>
2015-04-10 13:47:01 -0400

    added uniqueness checks

--- app/models/file_attachment.rb
+++ app/models/file_attachment.rb
@@ -3,6 +3,7 @@ require 'digest/sha1'
 class FileAttachment < ActiveRecord::Base
 
   before_validation :update_sha_1_hash
+  validates_uniqueness_of :sha_1_hash
 
   mount_uploader :file, FileUploader
   belongs_to :course
--- spec/models/file_attachment_spec.rb
+++ spec/models/file_attachment_spec.rb
@@ -10,4 +10,12 @@ describe FileAttachment do
     expect(subject.sha_1_hash).to eql('a8fdc205a9f19cc1c7507a60c4f01b13d11d7fd0')
   end
 
+  it 'should not allow dups' do
+    FactoryGirl.create(:file_attachment)
+
+    expect(subject.save).to eql(false)
+
+    
+  end
+
 end
