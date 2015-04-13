0c379402fead231363fff7b0949d70f1862736e1
John Maya <jcmaya@hotmail.com>
2015-04-13 18:31:11 -0400

    started a course file attachment sorter

---
+++ lib/course_sorter.rb
@@ -0,0 +1,9 @@
+class CourseSorter
+  attr_reader :list
+
+  def initialize list
+    @list = list
+  end
+
+
+end
---
+++ spec/lib/course_sorter_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+require 'course_sorter'
+
+describe CourseSorter do
+  #TODO: Finish these specs
+  context "with multiple items with dots..." do
+    let(:list) {
+      list = %w[
+      2.1_The_BDD_and_TDD_Cycle.mp4
+      2.10_Review.mp4
+      2.11_Assignment.mp4
+      2.2_The_First_Cucumber_Feature.mp4].collect do |n|
+        double(name:n)
+      end
+    }
+    let(:cs) { CourseSorter.new(list) }
+
+    it 'should start with a list of courses' do
+      expect(cs.list).to eql(list)
+    end
+
+    xit 'should sort the list with dots...' do
+      expect(cs.sort.first.name).to eql("2.1_The_BDD_and_TDD_Cycle.mp4")
+    end
+
+  end
+
+end
