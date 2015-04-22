29cb4851a384ebf849962aaa9403514919cd8603
John Maya <jcmaya@hotmail.com>
2015-04-22 13:51:07 -0400

    added default course root page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   get 'welcome/index'
-  root 'welcome#index'
+  root 'courses#index'
 
   resources :books
   resources :tags, only:[:index, :destroy]
