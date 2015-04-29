cbf477d19372010bffad2b30715624692fae4ebf
John Maya <jcmaya@hotmail.com>
2015-04-29 12:41:43 +0000

    added full docker-compose file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
-redis_domain = ENV['REDIS_PORT_6379_TCP_ADDR']  
-redis_port   = ENV['REDIS_PORT_6379_TCP_PORT']
+redis_domain = ENV['REDIS_PORT_6379_TCP_ADDR'] || 'localhost'
+redis_port   = ENV['REDIS_PORT_6379_TCP_PORT'] || '6379'
 
 if redis_domain && redis_port  
   redis_url = "redis://#{redis_domain}:#{redis_port}"
