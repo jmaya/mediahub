0094afec17ccefdce733296d987710c68a652a31
John Maya <jcmaya@hotmail.com>
2015-05-06 16:24:20 -0400

    fixed guard

--- lib/course_sorter.rb
+++ lib/course_sorter.rb
@@ -7,6 +7,7 @@ class CourseSorter
 
   def sort
     course.file_attachments.sort_by do |file_attachment|
+      return 0 if file_attachment.file_basename.nil?
       file_attachment.file_basename.scan(/\d+/).join.to_i
     end
   end
