01c1d3e147351ac33c7a30b2855fb89560f54711
John Maya <jcmaya@hotmail.com>
2015-05-18 19:37:07 -0400

    Rspec api test

--- spec/factories/file_attachment.rb
+++ spec/factories/file_attachment.rb
@@ -1,4 +1,7 @@
 require 'tempfile'
+File.open(Rails.root + "spec" + "fixtures" + "test.mp4", "w") do |f|
+  f.write("This is a test.")
+end
 FactoryGirl.define do
   factory :file_attachment do
     file File.open(Rails.root + "spec" + "fixtures" + "test.mp4")
--- spec/requests/file_attachments_api_spec.rb
+++ spec/requests/file_attachments_api_spec.rb
@@ -1,11 +1,24 @@
 require "rails_helper"
 
 RSpec.describe "File Attachment API", :type => :request do
+  before(:each) do
+  end
 
   it "creates a Widget and redirects to the Widget's page" do
     get "/api/v1//file_attachments/exists?hash=xxxxx"
     expect(response.status).to eql(404)
     expect(JSON.parse(response.body)["error"]).to eql("File Attachment not found")
+  end
+
+  it 'should respond with a 200 if the record exists' do
+    File.open(Rails.root + "spec" + "fixtures" + "test.mp4", "w") do |f|
+      f.write("This is a test.")
+    end
+
+    @file_attachment = create(:file_attachment)
+    get "/api/v1//file_attachments/exists?hash=#{@file_attachment.sha_1_hash}"
+    expect(response.status).to eql(200)
+    expect(JSON.parse(response.body)["sha_1_hash"]).to eql("afa6c8b3a2fae95785dc7d9685a57835d703ac88")
   end
 
 end
