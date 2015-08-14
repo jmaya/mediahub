e2dfad7b0f58d217aaa389fba6612192a372ca6b
John Maya <jcmaya@hotmail.com>
2015-08-14 16:49:30 -0400

    fixed sorting

--- lib/course_sorter.rb
+++ lib/course_sorter.rb
@@ -1,13 +1,16 @@
 class CourseSorter
   attr_reader :course
 
-  def initialize course
+  def initialize(course)
     @course = course
   end
 
   def sort
     course.file_attachments.to_a.sort_by do |file_attachment|
-      file_attachment.file_basename.scan(/\d+/).first.to_i unless file_attachment.file_basename.nil?
+      unless file_attachment.file_basename.nil?
+        file_to_match = file_attachment.file_basename.gsub(/\..{3}$/, '')
+        file_to_match.scan(/\d+/).collect(&:to_i)
+      end
     end
   end
 end
--- spec/lib/course_sorter_spec.rb
+++ spec/lib/course_sorter_spec.rb
@@ -3,34 +3,51 @@ require 'spec_helper'
 require 'course_sorter'
 
 describe CourseSorter do
-  #TODO: Finish these specs
-  context "with multiple items with dots..." do
-    let(:course) { double("Course") }
+  # TODO: Finish these specs
+  context 'with multiple items with dots...' do
+    let(:course) { double('Course') }
     let(:cs) { CourseSorter.new(course) }
 
     it 'should start with a list of courses' do
       expect(cs.course).to eql(course)
     end
 
-    context "sorts multiple attachments that have numbers in the start of name" do
+    context 'sorts multiple attachments numbers in the start of name' do
       it 'sorts by the first' do
         fas = [
-          double("FileAttachment", file_basename:'002.Getting-Started.mp3'),
-          double("FileAttachment", file_basename:'001.Getting-Started.mp3')
+          double('FileAttachment',
+                 file_basename: '002.Getting-Started.mp3'),
+          double('FileAttachment',
+                 file_basename: '001.Getting-Started.mp3')
         ]
         allow(course).to receive(:file_attachments).and_return(fas)
         expect(cs.sort[0]).to eql fas.last
       end
       it 'sorts by the first with 0 in the name' do
         fas = [
-          double("301_Safe_Proxy_Part_2.mp4", file_basename:'301_Safe_Proxy_Part_2.mp4'),
-          double("300_Safe_Proxy_Part_1.mp4", file_basename:'300_Safe_Proxy_Part_1.mp4'),
-          double("091_Ruby_2.0__Rebinding_Methods.mp4", file_basename:'091_Ruby_2.0__Rebinding_Methods.mp4')
+          double('301_Safe_Proxy_Part_2.mp4',
+                 file_basename: '301_Safe_Proxy_Part_2.mp4'),
+          double('300_Safe_Proxy_Part_1.mp4',
+                 file_basename: '300_Safe_Proxy_Part_1.mp4'),
+          double('091_Ruby_2.0__Rebinding_Methods.mp4',
+                 file_basename: '091_Ruby_2.0__Rebinding_Methods.mp4')
         ]
         allow(course).to receive(:file_attachments).and_return(fas)
         expect(cs.sort[0]).to eql fas[-1]
       end
     end
-  end
 
+    it 'Ignores extension' do
+      fas = [
+        double('01_07-Prominent_Users_of_PostgreSQL.mp4',
+               file_basename: ' 01_07-Prominent_Users_of_PostgreSQL.mp4'),
+        double('01_12-Summary.mp4',
+               file_basename: '01_12-Summary.mp4'),
+        double('01_04-What_Is_PostgreSQL.mp4',
+               file_basename: '01_04-What_Is_PostgreSQL.mp4')
+      ]
+      allow(course).to receive(:file_attachments).and_return(fas)
+      expect(cs.sort[0]).to eql fas[-1]
+    end
+  end
 end
