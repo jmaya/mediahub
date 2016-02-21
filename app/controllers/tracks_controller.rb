4f96a177b1d15ec50c788cafa49a6577d436d52a
John Maya <jcmaya@hotmail.com>
2016-02-21 11:07:26 -0500

    Added track course show

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,4 +2,9 @@ class TracksController < ApplicationController
   def index
     @tracks = Track.all
   end
+
+  def show
+    @track = Track.find params[:id]
+    @track_courses = @track.track_courses
+  end
 end
