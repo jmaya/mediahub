afc04b68d7b4363688782d073c828ad651d3d0f8
John Maya <jcmaya@hotmail.com>
2016-10-24 20:40:28 -0400

    Added video controls

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -19,17 +19,18 @@
     source: tags.ttAdapter()
   });
 
-  // var video = $('video');
-  // videojs('example_video_1', {
-  //   plugins: {
-  //     speed: [
-  //       { text: '1', rate: 1, selected: true },
-  //       { text: '2', rate: 2 },
-  //       { text: '4', rate: 4 },
-  //       { text: '8', rate: 8 }
-  //     ]
-  //   }
+  // videojs('video', {
+  //   // plugins: {
+  //   //   speed: [
+  //   //     { text: '1', rate: 1},
+  //   //     { text: '2', rate: 2, selected: true },
+  //   //     { text: '4', rate: 4 },
+  //   //     { text: '8', rate: 8 }
+  //   //   ]
+  //   // },
+  //  "controls": true,
+  //  "preload":"none",
+  //  "autoplay":false,
+  //  "playbackRates": [0.5, 1, 1.5, 2]
   // });
-  //
-
 })()
