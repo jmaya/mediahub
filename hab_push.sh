2f5d0fc1b632371cc6a757a8debb0bcdeb0ca895
John Maya <jcmaya@hotmail.com>
2017-09-19 12:06:08 -0400

    Dont build docker

--- hab_push.sh
+++ hab_push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-hab studio run "build . && hab pkg export docker jmaya/mediahub" && source results/last_build.env && hab pkg upload results/$pkg_artifact
+# hab studio run "build . && hab pkg export docker jmaya/mediahub" && source results/last_build.env && hab pkg upload results/$pkg_artifact
+hab studio run "build ." && source results/last_build.env && hab pkg upload results/$pkg_artifact
