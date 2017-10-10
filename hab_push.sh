0abce688ab2ff5cad698fa82c54f38112839f500
John Maya <jcmaya@hotmail.com>
2017-10-10 11:02:12 -0400

    Updated documentation

--- hab_push.sh
+++ hab_push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# This should not be used.  It is now using the build services from habitat.
 # hab studio run "build . && hab pkg export docker jmaya/mediahub" && source results/last_build.env && hab pkg upload results/$pkg_artifact
 hab studio run "build ." && source results/last_build.env && hab pkg upload results/$pkg_artifact
